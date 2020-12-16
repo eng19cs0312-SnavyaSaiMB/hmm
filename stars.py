fe93db496e2db6b2898054de632dfd63ec66a3a8
eng19cs0312-SnavyaSaiMB <76058162+eng19cs0312-SnavyaSaiMB@users.noreply.github.com>
2020-12-16 19:41:13 +0530

    Add files via upload

---
+++ stars.py
@@ -0,0 +1,7 @@
+def show_stars(rows):
+    for i in range(1,rows+1):
+        for j in range(1,i+1):
+            print("*",end="")
+        print("\n")
+r= int(input("Enter the number of rows"))
+show_stars(r)
